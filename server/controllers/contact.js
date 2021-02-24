57c8522f44cd9f2c6cd9df995aa197e75a91f84c
Chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2021-02-24 20:46:11 +0530

    Added Controller folder and modified files accordingly

---
+++ server/controllers/contact.js
@@ -0,0 +1,94 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// create a reference to the model
+let Contact = require('../models/businessContacts');
+
+module.exports.displayContactList = (req, res, next) => {
+    Contact.find((err, ContactList) => {
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            //console.log(ContactList);
+
+            res.render('business/list', {title: 'Business contacts', ContactList: ContactList});      
+        }
+    });
+}
+
+module.exports.displayAddPage = (req, res, next) => {
+    res.render('business/add', {title: 'Add Contact'})          
+}
+
+module.exports.processAddPage =  (req,res,next)=>{
+    let newContact = Contact({
+        "name":req.body.name,
+        "number":req.body.number,
+        "email":req.body.email
+    })
+    Contact.create(newContact,(err,Contact)=>{
+        if(err){
+            console.log(err);
+            res.end(err)
+        }
+        else{
+            res.redirect('/businessContacts')
+        }
+    })
+}
+
+module.exports.displayEditPage = (req,res,next)=>{
+    let id = req.params.id;
+
+    Contact.findById(id,(err, contactToEdit)=>{
+        if(err){
+            console.log(err);
+            res.end(err)
+        }
+        else{
+            res.render('business/edit',{title:'Edit Contact',contact: contactToEdit})
+
+        }
+    })
+}
+
+module.exports.processEditPage = (req,res,next)=>{
+    let id = req.params.id;
+
+    let updatedcontact = Contact({
+        "_id":id,
+        "name":req.body.name,
+        "number":req.body.number,
+        "email":req.body.email 
+    })
+    Contact.updateOne({_id:id},updatedcontact,(err)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/businessContacts')
+        }
+    })
+}
+
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
+
+    Contact.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+             // refresh the book list
+             res.redirect('/businessContacts');
+        }
+    });
+}
--- server/routes/businessContacts.js
+++ server/routes/businessContacts.js
@@ -1,102 +1,32 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
 // connect to our Book Model
-let Contact = require('../models/businessContacts');
+let Contact = require("../models/businessContacts");
+let contactController = require("../controllers/contact");
 
 /* GET Route for the Book List page - READ OPeration */
-router.get('/', (req, res, next) => {
-    Contact.find((err, contactList) => {
-        if(err)
-        {
-            return console.error(err);
-        }
-        else
-        {
-            //console.log(bookList);
-
-            res.render('business/list', {title: 'Business contacts', ContactList: contactList})            
-        }
-    });
-});
+router.get("/", contactController.displayContactList);
 
 /*Adding Add page - Create operation */
-router.get('/add', (req,res,next)=>{
-    res.render('business/add', {title: 'Add Contact'}) 
-})
+router.get("/add", contactController.displayAddPage);
+
+
 /*Post for Add page - Create Operation */
-router.post('/add', (req,res,next)=>{
-    let newContact = Contact({
-        "name":req.body.name,
-        "number":req.body.number,
-        "email":req.body.email
-    })
-    Contact.create(newContact,(err,Contact)=>{
-        if(err){
-            console.log(err);
-            res.end(err)
-        }
-        else{
-            res.redirect('/businessContacts')
-        }
-    })
-})
+router.post("/add", contactController.processAddPage);
+
+
 /*Adding Edit page - Update operation */
+router.get("/edit/:id", contactController.displayEditPage);
 
-router.get('/edit/:id', (req,res,next)=>{
-    let id = req.params.id;
 
-    Contact.findById(id,(err, contactToEdit)=>{
-        if(err){
-            console.log(err);
-            res.end(err)
-        }
-        else{
-            res.render('business/edit',{title:'Edit Contact',contact: contactToEdit})
-
-        }
-    })
-})
 /*Post for Edit page - Update Operation */
 
-router.post('/edit/:id', (req,res,next)=>{
-    let id = req.params.id;
-
-    let updatedcontact = Contact({
-        "_id":id,
-        "name":req.body.name,
-        "number":req.body.number,
-        "email":req.body.email 
-    })
-    Contact.updateOne({_id:id},updatedcontact,(err)=>{
-        if(err){
-            console.log(err);
-            res.end(err);
-        }
-        else{
-            res.redirect('/businessContacts')
-        }
-    })
-})
+router.post("/edit/:id", contactController.processEditPage);
 
 /* Deltet page - Delete Operation*/
 
-router.get('/delete/:id',(req, res, next) => {
-    let id = req.params.id;
-
-    Contact.remove({_id: id}, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        else
-        {
-             // refresh the book list
-             res.redirect('/businessContacts');
-        }
-    });
-});
+router.get("/delete/:id", contactController.performDelete);
 
 module.exports = router;
