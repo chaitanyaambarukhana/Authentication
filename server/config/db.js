2c573acfa2b6c68cff46e6dadc8d742dbc6d8506
Chaitanyaambarukhana <chaitanyaambarukhana23@gmail.com>
2021-03-01 01:29:59 +0530

    added mongodb atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,5 @@
-module.exports = { URI: "mongodb://localhost/businessContacts" }; //mongodb link to connecto for mongoose
+module.exports = 
+//mongodb link to connecto for mongoose
+{ //URI: "mongodb://localhost/businessContacts"
+URI : "mongodb+srv://chaitanya:PkAi2QDFtFI425yS@cluster0.e8ef8.mongodb.net/businessContacts?retryWrites=true&w=majority"
+ }; 
